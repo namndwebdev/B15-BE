c17d114569b0374fab3cf6af6cb694d131a4b38c
devVNduc <trongducstvn@gmail.com>
2024-03-21 15:09:25 +0700

    pull

--- configs/mongo.js
+++ configs/mongo.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 
 async function main() {
-    await mongoose.connect('mongodb+srv://admin:Admin%401234@cluster0.njqgn0s.mongodb.net/B15_BE');
+    await mongoose.connect('mongodb://127.0.0.1:27017/B15_BE');
+    console.log("da thanh cong")
 }
 
 main().catch(err => console.log(err));
